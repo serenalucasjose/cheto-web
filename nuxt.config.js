1ffbf4ac0b8716585687a03629330239f97a7bd5
lucas jose serena <serenalucasjose@gmail.com>
2021-12-11 20:59:16 -0300

    added og ttags twitter

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,14 +11,19 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Electronica / Indie / Pop' },
       { name: 'format-detection', content: 'telephone=no' },
       // Facebook related tags
       { hid: 'og:title', name: 'og:title', content: 'Cheto - Web Oficial' },
       { hid: 'og:image', name: 'og:image', content: '/images/main-bg-1.jpg' },
       { hid: 'og:site_name', name: 'og:name', content: 'Cheto - Web Oficial' },
+      { hid: 'og:description', name: 'og:description', content: 'Electronica / Indie / Pop' },
       // Twitter related tags
-      { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' }
+      { hid: 't-type', name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'Banda de Electronica / Indie / Pop, conocenos.' },
+      { hid: 'twitter:image', name: 'twitter:image', content: '/images/main-bg-1.jpg' },
+      { hid: 'twitter:player', name: 'twitter:player', content: 'https://open.spotify.com/embed/artist/3Zm7zIp9kIF6qvVCwXy6Gb' }
+
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
